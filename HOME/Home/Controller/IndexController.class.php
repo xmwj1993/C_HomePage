46d24942fe526a26921ef90d3b1332e581e7d634
JarrarOS <757521389@qq.com>
2016-03-28 18:36:15 +0800

    first commit

---
+++ HOME/Home/Controller/IndexController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+<<<<<<< HEAD
+        
+=======
+
+    	
+>>>>>>> f1769d8067f994c875025d1481e85c67827c1b57
+    	$this -> display();
+    	// $this -> display("test");
+
+
+    }
+}
